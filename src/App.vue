66b46718e24a208745b250368623156e6f00ce87
Deson <947123643@qq.com>
2017-12-30 02:47:52 +0800

    看了菜鸟的文档写的

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,107 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+<div id="app">
+  <img src="./assets/logo.png">
+  <h1>{{ msg }}</h1>
+  <div v-html="message"></div>
+  <hellocomponent></hellocomponent>
+
+  <label for="r1">修改颜色</label><input type="chexbox" v-model="class1" id="r1">
+  <br><br>
+  <div v-bind:class="{'class1':class1}">
+    directiva v-bind:class
   </div>
+
+  {{5+5}}<br> {{ ok ? 'YES' : 'NO' }}<br> {{ message.split('').reverse().join('') }}
+  <div v-bind:id="'list-' + id">菜鸟教程</div>
+
+  <p v-if="seen">现在你看到我了</p>
+
+  <pre><a v-bind:href="url">菜鸟教程</a></pre>
+
+  <p>{{ message }}</p>
+  <input v-model="message">
+
+   <!-- 完整语法 -->
+   <button v-on:click="reverseMessage">反转字符串</button>
+   <!-- 缩写 -->
+   <button @click="reverseMessage">反转字符串</button>
+
+
+   <!-- 在两个大括号中 -->
+{{ message | capitalize }}
+
+<!-- 在 v-bind 指令中 -->
+<div v-bind:id="rawId | formatId"></div>
+
+<!-- 完整语法 -->
+<a v-bind:href="url">菜鸟教程</a>
+<!-- 缩写 -->
+<a :href="url">菜鸟教程</a>
+
+</div>
 </template>
 
 <script>
+import hellocomponent from './components/hello.vue'
 export default {
   name: 'app',
-  data () {
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      message: '<h1>Vue.js学习</h1>',
+      class1: false,
+      ok: true,
+      message: 'runoob!',
+      id: 1,
+      seen: false,
+      url: 'http://www.runoob.com'
     }
+  },
+  components: {
+    hellocomponent
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  },
+  filters: {
+    capitalize: function (value) {
+      if (!value) return ''
+      value = value.toString()
+      return value.charAt(0).toUpperCase() + value.slice(1)
+    }
+
   }
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 
-h1, h2 {
-  font-weight: normal;
+h1,
+h2 {
+    font-weight: normal;
 }
 
 ul {
-  list-style-type: none;
-  padding: 0;
+    list-style-type: none;
+    padding: 0;
 }
 
 li {
-  display: inline-block;
-  margin: 0 10px;
+    display: inline-block;
+    margin: 0 10px;
 }
 
 a {
-  color: #42b983;
+    color: #42b983;
 }
 </style>
